173f9fc4304969aef2bd0217177d8f912baaef4e
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-05 22:20:02 +0300

    Add the tests

--- Part2.ClientServer/ArchitecturePatterns.Part2.ClientServer.Client.Tests/ClientTests.cs
+++ Part2.ClientServer/ArchitecturePatterns.Part2.ClientServer.Client.Tests/ClientTests.cs
@@ -4,5 +4,45 @@ using ArchitecturePatterns.Part2.ClientServer.Server;
 
 namespace ArchitecturePatterns.Part2.ClientServer.Client.Tests
 {
+    public class ClientProgramTests
+    {
+        private const int TestPort = 8889;
+        private ChatServer server;
+        private TcpClient client;
 
+        [SetUp]
+        public void Setup()
+        {
+            server = new ChatServer();
+            server.Start();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Stop();
+        }
+
+        [Test]
+        public void ClientProgram_ReceivesMessagesFromServer()
+        {
+            client = new TcpClient();
+            client.Connect("localhost", TestPort);
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            writer.AutoFlush = true;
+
+            string name = "TestClient";
+            writer.WriteLine(name);
+
+            writer.WriteLine("Server message");
+            string message = reader.ReadLine();
+
+            Assert.AreEqual("Server message", message);
+
+            reader.Close();
+            writer.Close();
+            client.Close();
+        }
+    }
 }
--- Part2.ClientServer/ArchitecturePatterns.Part2.ClientServer.Server.Tests/ChatServerTests.cs
+++ Part2.ClientServer/ArchitecturePatterns.Part2.ClientServer.Server.Tests/ChatServerTests.cs
@@ -3,3 +3,57 @@ using System.Text;
 
 namespace ArchitecturePatterns.Part2.ClientServer.Server.Tests;
 
+public class ChatServerTests
+{
+    private const int TestPort = 8889;
+    private ChatServer server;
+    private TcpClient client1;
+    private TcpClient client2;
+
+    [SetUp]
+    public void Setup()
+    {
+        server = new ChatServer();
+        Thread serverThread = new Thread(server.Start);
+        serverThread.Start();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        server.RemoveClient(server.Clients[0]);
+        server.RemoveClient(server.Clients[0]);
+        server.Stop();
+    }
+
+    [Test]
+    public void BroadcastMessage_SendsMessageToAllClientsExceptSender()
+    {
+        client1 = new TcpClient();
+        client1.Connect("localhost", TestPort);
+        StreamReader reader1 = new StreamReader(client1.GetStream(), Encoding.ASCII);
+        StreamWriter writer1 = new StreamWriter(client1.GetStream(), Encoding.ASCII);
+        writer1.AutoFlush = true;
+
+        client2 = new TcpClient();
+        client2.Connect("localhost", TestPort);
+        StreamReader reader2 = new StreamReader(client2.GetStream(), Encoding.ASCII);
+        StreamWriter writer2 = new StreamWriter(client2.GetStream(), Encoding.ASCII);
+        writer2.AutoFlush = true;
+
+        writer1.WriteLine("Client 1 message");
+        string message1 = reader1.ReadLine();
+        string message2 = reader2.ReadLine();
+
+        Assert.AreEqual("Client 1 message", message2);
+        Assert.IsNull(message1);
+
+        reader1.Close();
+        writer1.Close();
+        client1.Close();
+
+        reader2.Close();
+        writer2.Close();
+        client2.Close();
+    }
+}
