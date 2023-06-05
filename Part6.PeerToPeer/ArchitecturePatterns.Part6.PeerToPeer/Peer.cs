5bd2e557e20ff503f5fd1203e6b00741f46c3260
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-05 23:34:45 +0300

    Implement the P2P pattern

---
+++ Part6.PeerToPeer/ArchitecturePatterns.Part6.PeerToPeer/Peer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitecturePatterns.Part6.PeerToPeer;
+
+public class Peer
+{
+    private string name;
+    private List<Peer> peers;
+
+    public Peer(string name)
+    {
+        this.name = name;
+        peers = new List<Peer>();
+    }
+
+    public void ConnectToPeer(Peer peer)
+    {
+        peers.Add(peer);
+    }
+
+    public void SendMessage(string message)
+    {
+        Console.WriteLine($"{name} sent message: {message}");
+        BroadcastMessage(message);
+    }
+
+    private void BroadcastMessage(string message)
+    {
+        foreach (Peer peer in peers)
+        {
+            peer.ReceiveMessage(message);
+        }
+    }
+
+    public void ReceiveMessage(string message)
+    {
+        Console.WriteLine($"{name} received message: {message}");
+    }
+}
+
---
+++ Part6.PeerToPeer/ArchitecturePatterns.Part6.PeerToPeer/Program.cs
@@ -0,0 +1,20 @@
+﻿using ArchitecturePatterns.Part6.PeerToPeer;
+
+var peer1 = new Peer("Peer1");
+var peer2 = new Peer("Peer2");
+var peer3 = new Peer("Peer3");
+var allPeers = new List<Peer> { peer1, peer2, peer3 };
+
+foreach (var peer in allPeers)
+{
+    foreach (var remainingPeer in allPeers.Where(p => !p.Equals(peer)))
+    {
+        peer.ConnectToPeer(remainingPeer);
+    }
+}
+
+peer1.SendMessage("Henlo");
+
+peer2.SendMessage("Haiiiii");
+
+peer3.SendMessage("Hiiiiiii");
