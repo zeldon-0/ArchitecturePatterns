f62d8e448be7a17a9b0ba4d3cd9d23eed057659e
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-06 22:12:39 +0300

    Add the MVC pattern tests

--- Part8.ModelViewController/ArchitecturePatterns.Part8.ModelViewController.Tests/CalculatorControllerTests.cs
+++ Part8.ModelViewController/ArchitecturePatterns.Part8.ModelViewController.Tests/CalculatorControllerTests.cs
@@ -3,5 +3,43 @@ using Moq;
 
 public class CalculatorControllerTests
 {
+    [Test]
+    public void Calculate_AddsTwoNumbers()
+    {
+        // Arrange
+        var a = 5;
+        var b = 3;
+        var operation = "+";
+        var expected = 8;
 
+        var model = new CalculatorModel();
+        var view = new CalculatorView();
+        var controller = new CalculatorController(model, view);
+
+        // Act
+        controller.Calculate(a, b, operation);
+
+        // Assert
+        Mock.Get(view).Verify(v => v.DisplayResult(expected), Times.Once());
+    }
+
+    [Test]
+    public void Calculate_DividesTwoNumbers()
+    {
+        // Arrange
+        var a = 10;
+        var b = 2;
+        var operation = "/";
+        var expected = 5;
+
+        var model = new CalculatorModel();
+        var view = new CalculatorView();
+        var controller = new CalculatorController(model, view);
+
+        // Act
+        controller.Calculate(a, b, operation);
+
+        // Assert
+        Mock.Get(view).Verify(v => v.DisplayResult(expected), Times.Once());
+    }
 }
