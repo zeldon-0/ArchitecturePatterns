cddf0c3672871612b3347f509e8b34e5668840ea
Vadym Lavrinenko <vadym.lavrinenko@ihsmarkit.com>
2023-06-06 22:51:32 +0300

    Add the Interpreter pattern implementation

---
+++ Part10.Interpreter/ArchitecturePatterns.Part10.Interpreter/Expression.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitecturePatterns.Part10.Interpreter;
+public abstract class Expression
+{
+    public abstract int Interpret();
+}
+
+public class Number : Expression
+{
+    private int value;
+
+    public Number(int value)
+    {
+        this.value = value;
+    }
+
+    public override int Interpret()
+    {
+        return value;
+    }
+}
+
+public class Addition : Expression
+{
+    private Expression leftOperand;
+    private Expression rightOperand;
+
+    public Addition(Expression leftOperand, Expression rightOperand)
+    {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+    }
+
+    public override int Interpret()
+    {
+        return leftOperand.Interpret() + rightOperand.Interpret();
+    }
+}
+
+public class Subtraction : Expression
+{
+    private Expression leftOperand;
+    private Expression rightOperand;
+
+    public Subtraction(Expression leftOperand, Expression rightOperand)
+    {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+    }
+
+    public override int Interpret()
+    {
+        return leftOperand.Interpret() - rightOperand.Interpret();
+    }
+}
---
+++ Part10.Interpreter/ArchitecturePatterns.Part10.Interpreter/Program.cs
@@ -0,0 +1,10 @@
+﻿using ArchitecturePatterns.Part10.Interpreter;
+
+var leftOperand = new Number(1);
+var rightOperand = new Number(5);
+var addition = new Addition(leftOperand, rightOperand);
+
+var outerLeftOperand = new Number(24);
+var subtraction = new Subtraction(outerLeftOperand, addition);
+
+Console.WriteLine($"The expression execution result is: {subtraction.Interpret()}");
